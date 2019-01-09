256a09a95996b615bf9129c8ab66d7e2c2f3d5b9
secmeant <patryk.wlazlyn@gmail.com>
2019-01-09 09:20:14 +0100

    rmi example with callback

--- lab07/rmi_example/ApplicationServer.java
+++ lab07/rmi_example/ApplicationServer.java
@@ -4,11 +4,27 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
+import java.util.ArrayList;
+
 public class ApplicationServer {
 	
+	ArrayList<Integer> data = new ArrayList<Integer>();
+
 	public static void main(final String... args) throws RemoteException{
+		ApplicationServer server = new ApplicationServer();
+
+		server.callback();
+		server.callback();
+		server.callback();
+
 		Registry registry = LocateRegistry.createRegistry(5099);
-		registry.rebind("secretMethod", new HelloServant());
+		registry.rebind("secretMethod", new HelloServant(server));
+
+	}
+
+	public void callback(){
+		System.out.println("Someone called remote method.");
+		this.data.add(this.data.size());
 	}
 
 }
--- lab07/rmi_example/HelloServant.java
+++ lab07/rmi_example/HelloServant.java
@@ -3,13 +3,26 @@ package rmi_example;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
+import java.util.ArrayList;
+
 public class HelloServant extends UnicastRemoteObject implements HelloService {
-	public HelloServant() throws RemoteException{
+	ApplicationServer parent;
+
+	public HelloServant(ApplicationServer parent) throws RemoteException{
 		super();
+		this.parent = parent;
 	}
 
 	public String echo(String input) throws RemoteException{
-		return "Super secret method. Ur input: " + input;
+		String ret =  "Super secret " + input + "\n";
+
+		for(Integer number : this.parent.data){
+			ret += Integer.toString(number);
+			ret += ", ";
+		}
+
+		this.parent.callback();
+		return ret;
 	}
 
 }
